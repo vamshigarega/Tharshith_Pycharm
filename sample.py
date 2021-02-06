f9ff6822b5c6ddda0993fbc3d9c98aba3b641e0c
Tharshith <66735119+Tharshith@users.noreply.github.com>
2021-02-06 08:26:07 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,5 +1,10 @@
-list=[1,2,3,5,6,7]
-print("list", *list)
-list=[8,9,10,11,12]
-print(*list)
-print("Hello")
+def fib(n):
+  if n==0;
+  return 0;
+  if(n==1 or n==2)
+    return 1;
+  return fib(n-1)+fib(n-2)
+
+a=int(input())
+for i in range(a):
+  print(fib(i))
